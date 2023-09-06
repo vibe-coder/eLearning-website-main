e2a2c9dbcdeab3c55a4e5d4bdea968a171a1c836
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-06 10:58:59 +0100

    Imported Section 1 component into App.js

--- e-learning-website/src/App.js
+++ e-learning-website/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Section1 from './components/Section1';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Section1/>
     </div>
   );
 }
