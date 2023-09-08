ffb0305930e04bcf345ded2419debac878b64a8a
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-08 11:46:33 +0100

    Imported About Us component in App.js

--- e-learning-website/src/App.js
+++ e-learning-website/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AboutUs from './components/AboutUs';
 import Client from './components/Client';
 import NavBar from './components/NavBar';
 import Section1 from './components/Section1';
@@ -9,6 +10,7 @@ function App() {
       <NavBar/>
       <Section1/>
       <Client/>
+      <AboutUs/>
     </div>
   );
 }
