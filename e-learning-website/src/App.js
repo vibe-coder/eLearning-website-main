856c5f0ba1f1e7173ebb262b018b6ce4e448d56f
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-11 01:06:14 +0100

    Imported Features component in app.js

--- e-learning-website/src/App.js
+++ e-learning-website/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import AboutUs from './components/AboutUs';
 import Client from './components/Client';
+import Features from './components/Features';
 import NavBar from './components/NavBar';
 import Section1 from './components/Section1';
 
@@ -11,6 +12,7 @@ function App() {
       <Section1/>
       <Client/>
       <AboutUs/>
+      <Features/>
     </div>
   );
 }
