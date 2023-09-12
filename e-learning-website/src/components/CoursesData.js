8fb74d6d43996ca88d9caaa0fa771bbf4de48df0
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-12 18:40:23 +0100

    Created Courses data

---
+++ e-learning-website/src/components/CoursesData.js
@@ -0,0 +1,43 @@
+const CoursesData = [
+  {
+    imaageURL: "laptop-1",
+    name: "Basics of JAVA",
+    level: "Beginner Level",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully",
+    price: "125.65",
+  },
+
+  {
+    imaageURL: "tab",
+    name: "OOP Fundamentals",
+    level: "Beginner Level",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully",
+    price: "15.65",
+  },
+
+  {
+    imaageURL: "computer-setup",
+    name: "Into to UI/UX",
+    level: "Beginner Level",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully",
+    price: "16.58",
+  },
+
+  {
+    imaageURL: "gaming",
+    name: "UI Certification",
+    level: "Beginner Level",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully",
+    price: "45.15",
+  },
+
+  {
+    imaageURL: "laptop-1",
+    name: "UX Certification",
+    level: "Beginner Level",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully",
+    price: "45.15",
+  },
+]
+
+export default CoursesData;
