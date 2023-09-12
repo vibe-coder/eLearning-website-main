feb4146e840273abef5eb1a15e8bc4000966f69f
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-12 20:21:06 +0100

    Created Blog Post Data

---
+++ e-learning-website/src/components/BlogPostData.js
@@ -0,0 +1,39 @@
+const BlogPostData = [
+  {
+    imageURL: "trello",
+    title: "Why Java is the best programming language?",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
+  },
+
+  {
+    imageURL: "newspaper",
+    title: "Why Java is the best programming language?",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
+  },
+
+  {
+    imageURL: "books",
+    title: "Why Java is the best programming language?",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
+  },
+
+  {
+    imageURL: "magazine",
+    title: "Why Java is the best programming language?",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
+  },
+
+  {
+    imageURL: "todo",
+    title: "Why Java is the best programming language?",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
+  },
+
+  {
+    imageURL: "magnifyer",
+    title: "Why Java is the best programming language?",
+    info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
+  },
+]
+
+export default BlogPostData;
