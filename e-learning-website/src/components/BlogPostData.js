c6dcea436b35eba7535498d2b8ba38f1bfba2110
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-29 14:07:46 +0100

    Added unique key to Blog Data

--- e-learning-website/src/components/BlogPostData.js
+++ e-learning-website/src/components/BlogPostData.js
@@ -1,35 +1,41 @@
 const BlogPostData = [
   {
+    id: 1,
     imageURL: "trello",
     title: "Why Java is the best programming language?",
     info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
   },
 
   {
+    id: 2,
     imageURL: "newspaper",
     title: "Why Java is the best programming language?",
     info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
   },
 
   {
+    id: 3,
     imageURL: "books",
     title: "Why Java is the best programming language?",
     info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
   },
 
   {
+    id: 4,
     imageURL: "magazine",
     title: "Why Java is the best programming language?",
     info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
   },
 
   {
+    id: 5,
     imageURL: "todo",
     title: "Why Java is the best programming language?",
     info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
   },
 
   {
+    id: 6,
     imageURL: "magnifyer",
     title: "Why Java is the best programming language?",
     info: "Java is a class-based object-oriented simple programming language. Though we can not consider it to be fully."
